62f69fa916bbe3a4be0b49b058cf3d0a282bc7bb
leyeto <adeleyeifaturoti@yahoo.com>
2021-02-18 02:00:16 +0000

    18/02/2021 @ 02:00

--- 23-for-loop-exercise/index.js
+++ 23-for-loop-exercise/index.js
@@ -1,6 +1,15 @@
-const myName = "John"; // SET YOUR NAME
+const myName = "Leye"; // SET YOUR NAME
 
-const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
+const visitors = [
+  "Alex",
+  "Mike",
+  "Sam",
+  "Shawn",
+  "Chloe",
+  "Peter",
+  "Justin",
+  "Jennifer",
+]; // Add at least 5 names
 
 /**
  * Exercise 1
@@ -12,6 +21,11 @@ const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
 
 // ========================
 const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
+function greetVisitors() {
+  for (var i = 0; i < visitors.length; i++) {
+    console.log(`Hi ${visitors[i]}, my name is ${myName}`);
+  }
+}
 
 /**
  * Exercise 2
@@ -21,6 +35,14 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  */
 
 // ========================
+function getElementIndex(number) {
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] === number) {
+      return i;
+    }
+  }
+  return "Item not found";
+}
 
 /**
  * Exercise 3
@@ -30,6 +52,14 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
 
 // ========================
 
+function total() {
+  var sum = 0;
+  for (var i = 0; i < numbers; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
 const numbersForIndexes = [6, 2, 16, 23, 84, 21, 9, 3]; // DON'T TOUCH THIS LINE :)
 
 /**
@@ -38,9 +68,29 @@ const numbersForIndexes = [6, 2, 16, 23, 84, 21, 9, 3]; // DON'T TOUCH THIS LINE
  * and returns a new array which includes each number + it's index
  */
 
+function addIndex() {
+  var addedArray = [];
+  for (var i = 0; i < numbers.length; i++) {
+    addedArray.push(numbers[i] + i);
+  }
+  return addedArray;
+}
+
 const forLowestNumber = [5, 234, 96, 34, -34, 0, 23]; // DON'T TOUCH THIS LINE :)
 /**
  * Exercise 5
  *  create function {lowestNumber} which will return the lowest integer
  * from {forLowestNumber}
  */
+
+function lowestNumber() {
+  var lowestNo = forLowestNumber[0];
+  for (var i = 0; i < forLowestNumber.length; i++) {
+    if (lowestNo > forLowestNumber[i]) {
+      lowestNo = forLowestNumber[i];
+    }
+  }
+  return lowestNo;
+}
+
+//Fail count 2
