895e2f15ca141b5265a166edd1001b8433cb213e
leyeto <adeleyeifaturoti@yahoo.com>
2021-05-06 03:54:15 +0100

    06/05/2021

--- 70-fs-exercise/index.js
+++ 70-fs-exercise/index.js
@@ -10,6 +10,19 @@
  * 1. if {filename} not provided, log an error "File name is missing"
  * 2. if path is incorrect, log an error "No such file or directory"
  */
+const fs = require("fs");
+const createJsonFileWithContent = (data, fileName) => {
+  if (fileName) {
+    const dataToJSON = JSON.stringify(data);
+    if (!fs.existsSync(`./data`)) {
+      console.log("Directory not found");
+      fs.mkdirSync("./data");
+      console.log("Directory created");
+    }
+    return fs.writeFileSync(`./data/${fileName}.json`, dataToJSON);
+  }
+  console.log("File name is missing");
+};
 
 /**
  * Exercise 2
@@ -23,6 +36,18 @@
  * 1. if path is incorrect, log an error "No such file or directory"
  */
 
+const readJsonFile = (fileName) => {
+  if (!fs.existsSync(`./data/${fileName}.json`)) {
+    console.error("No such file or directory");
+    return null;
+  } else {
+    const data = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
+    console.log(data);
+
+    return data;
+  }
+};
+
 /**
  * Exercise 3
  *
@@ -31,12 +56,27 @@
  * and add it the file {./data/[fileName].json} synchronously.
  */
 
+const updateFileWithContent = (data, fileName) => {
+  const dataFromFileName = readJsonFile(fileName);
+  const addingData = JSON.stringify({ ...dataFromFileName, ...data });
+  fs.writeFileSync(`./data/${fileName}.json`, addingData);
+  const dataAfter = readJsonFile(fileName);
+  console.log("Data after add", dataAfter);
+};
+
 /**
  * Exercise 4
  *
  * create a function {getFileSize} that will take {fileName} as argument and return
  * file size in bytes.
  */
+
+const getFileSize = (fileName) => {
+  const fileStats = fs.statSync(`./data/${fileName}.json`);
+  return fileStats.size;
+};
+
+getFileSize("SammyFile");
 
 /**
  * Exercise 5
@@ -49,6 +89,18 @@
  * if you get an error, log error message
  */
 
+const cloneJsonFile = (source, destination) => {
+  if (fs.existsSync(`./data/${source}.json`) && destination) {
+    const sourceData = JSON.parse(fs.readFileSync(`./data/${source}.json`));
+    const destinationData = JSON.stringify(sourceData);
+    fs.writeFileSync(`./data/${destination}.json`, destinationData);
+  } else {
+    console.error("Source destination not found or destination not given");
+  }
+};
+
+cloneJsonFile("SammyFile");
+
 /**
  * Exercise 6
  *
@@ -60,3 +112,11 @@
  * if you get an error, log error message
  */
 
+const deleteFile = (fileToDelete) => {
+  try {
+    fs.unlinkSync(fileToDelete);
+    //file removed
+  } catch (err) {
+    console.error(err);
+  }
+};
